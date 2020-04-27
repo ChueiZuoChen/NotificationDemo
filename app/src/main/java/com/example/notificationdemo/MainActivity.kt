44ca2fd48137e6bd6d02ce1de7cecb6ed1bea018
czchen <louisa0103a@gmail.com>
2020-04-27 16:43:06 +0800

    Initial commit

---
+++ app/src/main/java/com/example/notificationdemo/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.notificationdemo
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+/*
+*   Notification
+*   1. 定義channelID -> package的名字
+* */
+
+class MainActivity : AppCompatActivity() {
+
+    private val channelID = "com.example.notificationdemo.channel1"
+    private var notificationManager: NotificationManager? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        createNotificationChannel(channelID, "DemoChannel", "This is a demo")
+        button.setOnClickListener {
+            displayNotification()
+        }
+    }
+
+    private fun displayNotification() {
+        val notificationId = 77
+        val notification = NotificationCompat.Builder(this, channelID)
+            .setContentTitle("Demo Title")
+            .setContentText("This is a demo notification")
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .build()
+        notificationManager?.notify(notificationId, notification)
+    }
+
+    private fun createNotificationChannel(id: String, name: String, channdelDescription: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(id, name, importance).apply {
+                description = channdelDescription
+            }
+            notificationManager?.createNotificationChannel(channel)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
